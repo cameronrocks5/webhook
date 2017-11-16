da87bb2b5ba5374569a79c32134e121c1befa20a
cameronrocks5 <cameronbud97@gmail.com>
2017-11-15 19:10:23 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var request = require('request');
+var bodyparser = require('bodyparser');
+
+var app = express();
+app.use(bodyparser.urlencoded({extended: true}));
+
+var webhook = prosses.env.DISCORD_WEBHOOK;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/webhook', (req, res) => {
+  request({
+    method: 'POST',
+    url.webhook,
+    json: {
+      "content": req.body.msg,
+      "username": "Huggu_Bot",
+   }
+  res.redirect
+});
+
+app.listen(80, () => {
+  console.log("server started")
+});
